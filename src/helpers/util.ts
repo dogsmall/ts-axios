e488e5757c001ddd96b96844d1f5041ea0b8d95f
郭世伟 <guosw@missfresh.cn>
2019-12-23 19:56:21 +0800

    feat(all): day1

---
+++ src/helpers/util.ts
@@ -0,0 +1,7 @@
+const toString = Object.prototype.toString
+export function isDate(val: any): boolean {
+  return toString.call(val) === '[object Date]'
+}
+export function isObject(val: any): val is Object {
+  return val !== null && typeof val === 'object'
+}
