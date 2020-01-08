c009d0a9686c7c0e02a62062d9a70e7bc03c05bd
郭世伟 <guosw@missfresh.cn>
2020-01-08 14:47:06 +0800

    feat:拦截器

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,10 +1,21 @@
-import { AxiosRequestConfig, AxiosPromise, Method, AxiosResponse } from '../types'
+import {
+  AxiosRequestConfig,
+  AxiosPromise,
+  Method,
+  AxiosResponse,
+  ResolvedFn,
+  RejectedFn
+} from '../types'
 import dispatchRequest from './dispatchRequest'
 import InterceptorManager from './interceptorManager'
 
 interface Interceptors {
   request: InterceptorManager<AxiosRequestConfig>
   response: InterceptorManager<AxiosResponse>
+}
+interface PromiseChain<T> {
+  resolved: ResolvedFn<T> | ((config: AxiosRequestConfig) => AxiosPromise)
+  rejected?: RejectedFn
 }
 export default class Axios {
   interceptors: Interceptors
@@ -24,13 +35,24 @@ export default class Axios {
     } else {
       config = url
     }
-    const chain = [
+    const chain: PromiseChain<any>[] = [
       {
         resolved: dispatchRequest,
         rejected: undefined
       }
     ]
-    return dispatchRequest(config!)
+    this.interceptors.request.forEach(interceptor => {
+      chain.unshift(interceptor)
+    })
+    this.interceptors.request.forEach(interceptor => {
+      chain.push(interceptor)
+    })
+    let promise = Promise.resolve(config)
+    while (chain.length) {
+      const { resolved, rejected } = chain.shift()!
+      promise = promise.then(resolved, rejected)
+    }
+    return promise
   }
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithoutData('get', url, config)
